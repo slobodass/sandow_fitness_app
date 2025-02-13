cd204aa40c6b805a095853cdb9bc2ffa562df2ec
slobadass <as8889937@gmail.com>
2025-02-14 00:32:59 +0300

    Добавил соцсети + мелочи

--- src/components/index.js
+++ src/components/index.js
@@ -19,3 +19,29 @@ trainingsModal.addEventListener('click', (event) => {
         trainingsModal.classList.remove('modal_active')
     }
 })
+
+const slider = document.querySelector('.trainings__list')
+const prevBtn = document.querySelector('.rigth-button')
+const nextBtn = document.querySelector('.rigth-button')
+// prevBtn.textContent = '←'
+// nextBtn.textContent = '→'
+// prevBtn.classList.add('slider-btn', 'prev')
+// nextBtn.classList.add('slider-btn', 'next')
+// slider.parentElement.appendChild(prevBtn)
+// slider.parentElement.appendChild(nextBtn)
+
+const scrollAmount = slider.children[0]. offsetWidth + 20;
+
+nextBtn.addEventListener('click', () => {
+    slider.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+    })
+})
+
+prevBtn.addEventListener('click', () => {
+    slider.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
+    })
+})
