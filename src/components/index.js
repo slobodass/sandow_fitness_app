339e1566453db51f8c470ab01b34c45bdd82856b
slobadass <as8889937@gmail.com>
2025-02-16 09:31:15 +0300

    изменили открытие модального окна

--- src/components/index.js
+++ src/components/index.js
@@ -3,14 +3,59 @@ const trainingsCards = document.querySelectorAll('.trainings__item')
 const trainingsModal = document.querySelector('.modal')
 const modalCloseButton = document.querySelector('.modal__button-close')
 
-trainingsCards.forEach((card) => {
+const cardData = [
+    {
+        title: 'Йога',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Кросс-тренинг',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Здоровая спина',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Dance hall',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Zumba',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Шпагат',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Стретчинг',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'Табата',
+        description: 'Йога - ..........'
+    },
+    {
+        title: 'TRX',
+        description: 'Йога - ..........'
+    },
+]
+const modalTitle = document.querySelector('.modal_title')
+const modalText = document.querySelector('.modal_text')
+
+
+trainingsCards.forEach((card, index) => {
     card.addEventListener('click', () => {
+        modalTitle.textContent = cardData[index].title
+        modalText.textContent = cardData[index].description
+
         trainingsModal.classList.add('modal_active')
     })
 })
 
 // крестик закрывает окно
-modalCloseButton.addEventListener('click', () =>  {
+modalCloseButton.addEventListener('click', () => {
     trainingsModal.classList.remove('modal_active')
 })
 
@@ -20,28 +65,5 @@ trainingsModal.addEventListener('click', (event) => {
     }
 })
 
-const slider = document.querySelector('.trainings__list')
-const prevBtn = document.querySelector('.rigth-button')
-const nextBtn = document.querySelector('.rigth-button')
-// prevBtn.textContent = '←'
-// nextBtn.textContent = '→'
-// prevBtn.classList.add('slider-btn', 'prev')
-// nextBtn.classList.add('slider-btn', 'next')
-// slider.parentElement.appendChild(prevBtn)
-// slider.parentElement.appendChild(nextBtn)
 
-const scrollAmount = slider.children[0]. offsetWidth + 20;
 
-nextBtn.addEventListener('click', () => {
-    slider.scrollBy({
-        left: scrollAmount,
-        behavior: 'smooth'
-    })
-})
-
-prevBtn.addEventListener('click', () => {
-    slider.scrollBy({
-        left: -scrollAmount,
-        behavior: 'smooth'
-    })
-})
